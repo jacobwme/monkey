a7ec5c06ec8aa3c634ce4a2e912ef5ad437c8bb5
Jacob Williams <mail@jacobw.me>
2018-04-01 09:53:53 +0100

    Page 141 / up to Functions & Funcion Calls
    Test errors:
    - evaluator_test.go:176
    
    REPL behaviour as expected

--- repl/repl.go
+++ repl/repl.go
@@ -7,12 +7,15 @@ import (
 	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
+	// "github.com/kardianos/govendor/context"
+	"monkey/object"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
